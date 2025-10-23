2f93a5fc1d25b61bf0c8732f86b968a83d6d46d3
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-10-23 17:48:33 +0200

    Segundo commit

---
+++ src/app/shared/components/navbar/nav-list/nav-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nav-list',
+  imports: [],
+  templateUrl: './nav-list.html',
+  styleUrl: './nav-list.scss',
+})
+export class NavList { }
--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavLogo } from "./nav-logo/nav-logo";
+import { NavList } from "./nav-list/nav-list";
 
 @Component({
   selector: 'navbar',
-  imports: [],
+  imports: [NavLogo, NavList],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
