1123d1a756fdc9632a60d5cbb8d7e050ed748c4f
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-10-24 13:58:51 +0200

    Navbar beta

--- src/app/shared/components/navbar/navbar.ts
+++ src/app/shared/components/navbar/navbar.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavLogo } from "./nav-logo/nav-logo";
 import { NavList } from "./nav-list/nav-list";
+import { NavLogin } from "./nav-login/nav-login";
 
 @Component({
   selector: 'navbar',
-  imports: [NavLogo, NavList],
+  imports: [NavLogo, NavList, NavLogin],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss',
 })
---
+++ src/app/shared/modules/shared.module.ts
@@ -0,0 +1,13 @@
+// app.module.ts
+import { NgModule } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+// ... otros imports
+
+@NgModule({
+  imports: [
+    // ...
+    MatIconModule
+  ],
+  // ...
+})
+export class SharedIconsModule { }
