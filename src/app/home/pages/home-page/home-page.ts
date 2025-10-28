5802c1900ca722c73e6b2638bf8fb0590495f069
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-10-28 11:25:18 +0100

    Primer mixin usado

--- src/app/home/pages/home-page/home-page.ts
+++ src/app/home/pages/home-page/home-page.ts
@@ -1,13 +1,12 @@
 import { Component } from '@angular/core';
-import { Navbar } from "../../../shared/components/navbar/navbar";
-import { HomeCartel } from "../../components/home-cartel/home-cartel";
-import { HomeOptions } from "../../components/home-options/home-options";
 import { Footer } from "../../../shared/components/footer/footer";
 import { NavbarWrapper } from "../../../shared/components/navbar/navbar-wrapper/navbar-wrapper";
+import { HomeCartel } from "../../components/home-cartel/home-cartel";
+import { HomeOptions } from "../../components/home-options/home-options";
 
 @Component({
   selector: 'app-home-page',
-  imports: [Navbar, HomeCartel, HomeOptions, Footer, NavbarWrapper],
+  imports: [ HomeCartel, HomeOptions, Footer, NavbarWrapper],
   templateUrl: './home-page.html',
   styleUrl: './home-page.scss',
 })
