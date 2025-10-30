7436902ad56b1910a284e958974a8339a48cb53c
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-10-30 18:20:32 +0100

    AAAAAAA

--- src/app/job/components/job-search-form/job-search-form.ts
+++ src/app/job/components/job-search-form/job-search-form.ts
@@ -1,15 +1,22 @@
 import { Component } from '@angular/core';
 import { MatSelectModule } from '@angular/material/select';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from "@angular/material/form-field";
 import { TipoContrato } from '../../enums/TipoContrato';
-
+import { ModalidadTrabajo } from '../../enums/ModalidadTrabajo';
+import { TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'job-search-form',
-  imports: [MatSelectModule],
+  standalone: true, // asegúrate de tener esto
+  imports: [
+    MatSelectModule,
+    TitleCasePipe
+  ],
   templateUrl: './job-search-form.html',
   styleUrl: './job-search-form.scss',
 })
 export class JobSearchForm { 
   tiposContratoValues = Object.values(TipoContrato);
-
+  tiposModalidadesValues = Object.values(ModalidadTrabajo);
 }
