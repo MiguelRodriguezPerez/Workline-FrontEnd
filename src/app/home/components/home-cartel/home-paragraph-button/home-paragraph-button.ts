6c258202c378774accfc034ddd2e24b546a7dac8
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-11-19 18:42:37 +0100

    Small changes

--- src/app/home/components/home-cartel/home-paragraph-button/home-paragraph-button.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'home-paragraph-button',
-  imports: [],
-  templateUrl: './home-paragraph-button.html',
-  styleUrl: './home-paragraph-button.scss',
-})
-export class HomeParagraphButton { }
