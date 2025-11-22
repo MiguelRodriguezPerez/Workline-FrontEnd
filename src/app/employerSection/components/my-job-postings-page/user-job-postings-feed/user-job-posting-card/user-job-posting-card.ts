7a6dc9e93b3bca759343e59df871da40ccc0df93
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-11-22 12:32:34 +0100

    Job card scss beta

--- src/app/employerSection/components/my-job-postings-page/user-job-postings-feed/user-job-posting-card/user-job-posting-card.ts
+++ src/app/employerSection/components/my-job-postings-page/user-job-postings-feed/user-job-posting-card/user-job-posting-card.ts
@@ -1,13 +1,26 @@
-import { Component, input } from '@angular/core';
+import { TitleCasePipe } from '@angular/common';
+import { Component, input, linkedSignal } from '@angular/core';
 import { Oferta } from '../../../../../shared/objects/interfaces/oferta/Oferta';
-import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'user-job-posting-card',
-  imports: [JsonPipe],
+  imports: [TitleCasePipe],
   templateUrl: './user-job-posting-card.html',
   styleUrl: './user-job-posting-card.scss',
 })
 export class UserJobPostingCard { 
+  
+  private titleCasePipe = new TitleCasePipe();
   jobPosting = input.required<Oferta>();
+  jobCardValues = linkedSignal(() => {
+    const resultado = {
+      salarioAnual: this.jobPosting().salarioAnual,
+      modalidadTrabajo: this.titleCasePipe.transform(this.jobPosting().modalidadTrabajo),
+      horas: this.jobPosting().horas,
+      ciudad: this.jobPosting().ciudad
+    }
+    return Object.values(resultado);
+  })
+
+
 }
