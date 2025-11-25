55cbab7a61269635b87bbb557c0dbec62301bf65
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-11-25 11:13:40 +0100

    my-job-postings && wk-paginator && wk-loading

--- src/app/employerSection/pages/my-job-postings-page/my-job-postings-page.ts
+++ src/app/employerSection/pages/my-job-postings-page/my-job-postings-page.ts
@@ -1,16 +1,19 @@
 import { Component, inject, signal } from '@angular/core';
-import { NavbarWrapper } from "../../../shared/components/navbar/navbar-wrapper/navbar-wrapper";
-import { Footer } from "../../../shared/components/footer/footer";
-import { JobPostingsCounter } from "../../components/my-job-postings-page/job-postings-counter/job-postings-counter";
-import { NewJobPostingButton } from "../../components/my-job-postings-page/new-job-posting-button/new-job-posting-button";
 import { rxResource } from '@angular/core/rxjs-interop';
 import { PaginaJobResponse } from '../../../jobSearch/objects/interfaces/PaginaJobResponse';
-import { ContrataService } from '../../service/contrata.service';
+import { Footer } from "../../../shared/components/footer/footer";
+import { NavbarWrapper } from "../../../shared/components/navbar/navbar-wrapper/navbar-wrapper";
+import { WkPaginator } from "../../../shared/components/wk-paginator/wk-paginator";
+import { JobPostingsCounter } from "../../components/my-job-postings-page/job-postings-counter/job-postings-counter";
+import { NewJobPostingButton } from "../../components/my-job-postings-page/new-job-posting-button/new-job-posting-button";
 import { UserJobPostingsFeed } from "../../components/my-job-postings-page/user-job-postings-feed/feed/user-job-postings-feed";
+import { ContrataService } from '../../service/contrata.service';
+import { JobSearchFeedLoading } from "../../../shared/components/wk-loading/wk-loading";
+import { NoJobPostings } from "../../components/my-job-postings-page/user-job-postings-feed/no-job-postings/no-job-postings";
 
 @Component({
   selector: 'my-job-postings-page',
-  imports: [NavbarWrapper, Footer, JobPostingsCounter, NewJobPostingButton, UserJobPostingsFeed],
+  imports: [NavbarWrapper, Footer, JobPostingsCounter, NewJobPostingButton, UserJobPostingsFeed, WkPaginator, JobSearchFeedLoading, NoJobPostings],
   templateUrl: './my-job-postings-page.html',
   styleUrl: './my-job-postings-page.scss',
 })
@@ -23,5 +26,9 @@ export class MyJobPostingsPage {
     params: () => this.currentPage(),
     stream: ({ params }) =>
       this.contrataService.getOfertasContrataPage(params)
-  })
+  });
+
+  changePage (page: number) {
+    this.currentPage.set(page);
+  } 
 }
--- src/app/jobSearch/components/job-search-page/job-search-feed/job-search-feed-wrapper/job-search-feed-wrapper.ts
+++ src/app/jobSearch/components/job-search-page/job-search-feed/job-search-feed-wrapper/job-search-feed-wrapper.ts
@@ -1,12 +1,13 @@
 import { Component, input } from '@angular/core';
 import { Oferta } from '../../../../../shared/objects/interfaces/oferta/Oferta';
 import { JobSearchFeed } from '../feed/job-search-feed';
-import { JobSearchFeedLoading } from "../job-search-feed-loading/job-search-feed-loading";
+
 import { JobSearchNoResults } from "../../job-search-no-results/job-search-no-results";
+import { JobSearchFeedLoading } from "../../../../../shared/components/wk-loading/wk-loading";
 
 @Component({
   selector: 'job-search-feed-wrapper',
-  imports: [JobSearchFeed, JobSearchFeedLoading, JobSearchNoResults],
+  imports: [JobSearchFeed, JobSearchNoResults, JobSearchFeedLoading],
   templateUrl: './job-search-feed-wrapper.html',
   styleUrl: './job-search-feed-wrapper.scss',
 })
