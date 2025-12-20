f01d5b9ac5f6b57ccc4c95ffd624661d70c22dca
Miguel Rodríguez <jazmindemar33@gmail.com>
2025-12-20 11:17:48 +0100

    Updated env imports

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+/* Se supone que aunque esto sea publico en github tu backend debería estar protegido por la seguridad
+de ataques ddos y otro tipo de maravillas, pero yo creo que esta url no debería ser visible en github */
+
+export const environment = {
+    production: true,
+    apiUrl: 'https://workline-backend-production.up.railway.app'
+};
